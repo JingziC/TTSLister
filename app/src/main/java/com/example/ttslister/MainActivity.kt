effdc16a7a9f05b97311d80e5f51d4ac54cc37d9
JingziC <utjingzichen@outlook.com>
2024-06-24 23:48:54 +0900

    Add files

---
+++ app/src/main/java/com/example/ttslister/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.ttslister
+
+import android.media.MediaDrm.ErrorCodes
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+    private lateinit var tts: TextToSpeech
+    private lateinit var enginesText: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        enginesText = findViewById(R.id.tts_engines_text)
+        tts = TextToSpeech(this, this,"com.google.android.tts")
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val engines = tts.engines
+            enginesText.text = "Available TTS Engines: $engines"
+        // Engineinfo{name="com.samsung.SMT" in Samsung} and cannot found google tts. Hope it works in other OS
+        }
+        else {
+            enginesText.text = "TTS Initialization failed!"
+        }
+    }
+
+    override fun onDestroy() {
+        if (tts.isSpeaking) tts.stop()
+        tts.shutdown()
+        super.onDestroy()
+    }
+}
